5bfc646cff0da42104243aad2ae69f4a232d8be1
DJ Kullas <dkoolj5@gmail.com>
2022-07-21 17:40:26 -0400

    check firebaseui-angular npm docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { CreativeCvComponent } from './creative-cv/creative-cv.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [{ path: '', component: LandingComponent },
-{ path: 'creative', component: CreativeCvComponent }];
+{ path: 'creative', component: CreativeCvComponent },
+{ path: 'auth', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
